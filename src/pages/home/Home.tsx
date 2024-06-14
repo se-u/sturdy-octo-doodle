ea7a11f82da7536f3686ae596103a4c4d70c444d
Azriel Pamungkas <github.rwcoy@simplelogin.co>
2024-06-14 16:57:26 +0700

    rebase with dev fix error on home  interactive rebase in progress; onto 8fd3b76  Last command done (1 command done):     pick f7aa968 rebase with dev  Next command to do (1 remaining command):     pick 94c0206 update appwrite  You are currently rebasing branch 'main' on '8fd3b76'.
     Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	new file:   src/@types/poin.d.ts
    	new file:   src/context/poinContext.tsx
    	new file:   src/lib/CreateRepository.ts
    	deleted:    src/lib/context/poins.tsx
    	modified:   src/main.tsx
    	modified:   src/pages/Calculator.tsx
    	modified:   src/pages/TestAppWrite.tsx
    	modified:   src/pages/home/Home.tsx
    	deleted:    yarn.lock

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,17 @@
 import { Link } from "react-router-dom";
-import { BlueReceiveIcon, DanaIcon, GopayIcon, OvoIcon, ReceiveIcon, convertCoin } from "../../components/Image";
+import {
+  BlueReceiveIcon,
+  DanaIcon,
+  GopayIcon,
+  OvoIcon,
+  ReceiveIcon,
+  convertCoin,
+} from "../../components/Image";
 
 export default function Home() {
   return (
     <>
       {/* Statistic Start */}
-<<<<<<< HEAD
       <div className="bg-green-200 border-2 border-black p-4 mb-4 rounded-xl flex justify-between">
         <div className="">
           <h2 className="text-sm font-bold text-black ">Klora Point</h2>
@@ -24,17 +30,6 @@ export default function Home() {
             alt="Tas"
             width={125}
           />
-=======
-      <div className="border-2 bg-green border-black p-2 mb-4 rounded-3xl flex gap-6">
-        <div className="flex gap-4 items-center">
-          <img src="/bags/1.png" className="rounded-2xl border-2 border-black shadow-md" alt="Tas" width={140} />
-        <div className=" pr-7 pl-2 ">
-          <h2 className="text-sm font-bold text-lightgreen">Total Poin</h2>
-          <p className="text-white text-3xl font-bold">100 Poin</p>         
-          <h2 className="text-sm font-bold text-lightgreen">Total Botol</h2>
-          <p className="text-white text-3xl  font-bold">20 Botol</p>
-        </div>
->>>>>>> 8fd3b76 (redesign: HomePage)
         </div>
       </div>
       {/* Statistic End */}
@@ -73,44 +68,43 @@ export default function Home() {
       {/* Riwayat Transaksi Start */}
       <div className="bg-green-200 border-2 border-black p-4 mb-4 rounded-3xl">
         <Link to="/app/transaction" className="flex justify-between">
-<<<<<<< HEAD
           <h2 className="text-sm font-bold text-black">Riwayat Transaksi</h2>
           <button className="py-1 px-3 border-2 items-center border-black rounded-full text-xs font-bold bg-yellow-300">
             Lihat semua 👉
           </button>
-=======
-          <h2 className="text-medium font-bold text-black" >Riwayat Transaksi</h2>
-          <button className="py-1 px-3 border-2 items-center border-black rounded-full text-xs font-bold bg-green text-white">Lihat semua</button>
->>>>>>> 8fd3b76 (redesign: HomePage)
         </Link>
         {/* Data Transaction */}
-       <div className="mt-5 py-2">
-
-       <div className="flex border-black justify-between  items-center pb-2 ">
-          <div className="flex flex-row gap-2  items-center">
-            <img src={ReceiveIcon} alt="tes" className="h-10" />
-            <span className="font-medium text-lg">Poin Klora</span>
+        <div className="mt-5 py-2">
+          <div className="flex border-black justify-between  items-center pb-2 ">
+            <div className="flex flex-row gap-2  items-center">
+              <img src={ReceiveIcon} alt="tes" className="h-10" />
+              <span className="font-medium text-lg">Poin Klora</span>
+            </div>
+            <span className="font-semibold text-lightgray text-medium">
+              + 50 Poin
+            </span>
           </div>
-          <span className="font-semibold text-lightgray text-medium">+ 50 Poin</span>
-        </div>
 
-        <div className="flex border-black justify-between  items-center pb-2 ">
-          <div className="flex flex-row gap-2  items-center">
-            <img src={BlueReceiveIcon} alt="tes" className="h-10" />
-            <span className="font-medium text-lg">Dana</span>
+          <div className="flex border-black justify-between  items-center pb-2 ">
+            <div className="flex flex-row gap-2  items-center">
+              <img src={BlueReceiveIcon} alt="tes" className="h-10" />
+              <span className="font-medium text-lg">Dana</span>
+            </div>
+            <span className="font-semibold text-lightgray text-medium">
+              -Rp64.000
+            </span>
           </div>
-          <span className="font-semibold text-lightgray text-medium">-Rp64.000</span>
-        </div>
 
-        <div className="flex border-black justify-between  items-center pb-2 ">
-          <div className="flex flex-row gap-2  items-center">
-            <img src={BlueReceiveIcon} alt="tes" className="h-10" />
-            <span className="font-medium text-lg">Dana</span>
+          <div className="flex border-black justify-between  items-center pb-2 ">
+            <div className="flex flex-row gap-2  items-center">
+              <img src={BlueReceiveIcon} alt="tes" className="h-10" />
+              <span className="font-medium text-lg">Dana</span>
+            </div>
+            <span className="font-semibold text-lightgray text-medium">
+              -Rp64.000
+            </span>
           </div>
-          <span className="font-semibold text-lightgray text-medium">-Rp64.000</span>
         </div>
-
-       </div>
       </div>
       {/* Riwayat Transaksi End */}
 
