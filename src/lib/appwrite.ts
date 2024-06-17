bc255f672a3a696d7842843d2e24299e62e5b91b
Azriel Pamungkas <github.rwcoy@simplelogin.co>
2024-06-17 07:23:18 +0700

    Feature: login using phonenumber

--- src/lib/appwrite.ts
+++ src/lib/appwrite.ts
@@ -1,9 +1,8 @@
-import { Client, Databases, Account } from "appwrite";
+import { Account, Client, Databases } from "appwrite";
+import { ENDPOINT, PROJECT_ID } from "../modules/constant";
 const client = new Client();
 
-client
-    .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject('666846db000c6c3d312a');
+client.setEndpoint(ENDPOINT).setProject(PROJECT_ID);
 
 export const account = new Account(client);
 export const databases = new Databases(client);
