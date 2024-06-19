44b6dd7efd8e38843d43da79b3b92009ef30dc6c
SinduAditya <77913475+Sinduaditya@users.noreply.github.com>
2024-06-19 21:23:32 +0700

    converting menu (#11)

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import Calculator from "./pages/Calculator.tsx";
 import Shop from "./pages/Shopping.tsx";
 import Transaction from "./components/transaction/Transaction.tsx";
 import Wallet from "./pages/Wallet.tsx";
-import Converting from "@components/convert/converting.tsx";
 import SwapPoint from "@components/convert/SwapPoint.tsx";
 import Login from "@components/login/Login.tsx";
 import Setup from "@components/setup/Setup.tsx";
@@ -18,6 +17,7 @@ import {
   MobileLayout,
   MobileLayoutMinimal,
 } from "@components/layouts/mobile/MobileLayout.tsx";
+import ConvertList from "@components/convert/ConvertList.tsx";
 
 const router = createBrowserRouter([
   {
@@ -48,8 +48,8 @@ function Dashboard() {
       <Route path="calculator" element={<Calculator />} />
       <Route path="shop" element={<Shop />} />
       <Route path="transaction" element={<Transaction />} />
-      <Route path="convert" element={<Converting />} />
-      <Route path="swapPoint" element={<SwapPoint />} />
+      <Route path="convert/:type" element={<ConvertList />} />
+      <Route path="swapPoint/:type/:amount" element={<SwapPoint />} />
       <Route path="wallet/" element={<Wallet />} />
     </Routes>
   );
